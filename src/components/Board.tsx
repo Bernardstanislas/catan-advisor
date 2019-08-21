3526e31e13775a5fb96d92d230184dbe102efb7c
Stanislas Bernard <bernardstanislas@gmail.com>
2019-08-21 14:31:46 +0200

    :alembic: Add fix polygon

---
+++ src/components/Board.tsx
@@ -0,0 +1,5 @@
+import * as React from 'react';
+
+export default ({children}) => (
+    <svg className="w-full">{children}</svg>
+)
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,10 @@
 import * as React from "react"
+import Board from '../components/Board';
+import '../style.css'
 
-export default () => <div>Hello world!</div>
+export default () => <div className="container mx-auto p-2 bg-gray-100 h-screen">
+    <h1 className="text-xl text-grey-500 text-center">Catan Advisor</h1>
+    <Board>
+        <polygon points="60,20 100,40 100,80 60,100 20,80 20,40" fill="red"/>
+    </Board>
+</div>
